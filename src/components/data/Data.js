2577f18dc6bba12f1517b0e38827097d0021cb22
Kapil Ray <kapileshworray@gmail.com>
2023-07-05 23:01:43 +0530

    construct app

--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -22,45 +22,50 @@ export const featured = [
   {
     cover: "../images/hero/h1.png",
     name: "Civil Works",
+    total: "Includes construction, maintenance, and improvement of infrastructure projects that are essential for the functioning of a society such as roads, bridges, dams, airports, railways, canals, pipelines, and buildings.",
   
    
   },
   {
     cover: "../images/hero/h2.png",
     name: "Restoration",
-   
+    total: "Includes the design, engineering, procurement, construction and other work with respect to the Restoration of Affected Property.",
   },
   {
     cover: "../images/hero/h3.png",
     name: "Structural Repair",
-    
+    total: "Includes the repairing of foundation, wall and roof support columns, roof joists and the roof deck of the Building.",
   },
   {
     cover: "../images/hero/h4.png",
     name: "Waterproofing",
-    
+    total: "Involves applying specialized materials or coatings to prevent water penetration and protect against water damage.",
   },
   {
     cover: "../images/hero/h6.png",
     name: "Plumbing",
-   
+    total: "Invloves installation, repair, and maintenance of plumbing systems to ensure proper functioning of water supply and wastewater management.",
   },
   {
     cover: "../images/hero/h7.png",
     name: "Fabrication",
+    total: "Involves constructing products by combining typically standardised parts using one or more individual processes.",
   },
     
   {
     cover: "../images/hero/h8.png",
     name: "PCC & POP & RCC",
+    total: "PCC  is used as a foundational layer in construction. POP is used for creating smooth surfaces on walls and ceilings. RCC is a composite material combining concrete and steel reinforcement bars for added strength in construction projects.",
   },
   {
     cover: "../images/hero/h9.png",
     name: "Printing Works",
+    total: "Involves covering the surfaces of wood, metal, masonry, plastered walls, slabs, etc. To protect the surfaces from atmospheric influences and to preserve them from decay, oxidation, and corrosion.",
   },
   {
     cover: "../images/hero/h10.png",
     name: "New Swimming Pool & Service",
+    total: "Involves designing and building a new swimming pool involves creating a customized layout and structure that meets the specific requirements and preferences of the client." ,
   },
   
  
--- src/components/home/featured/FeaturedCard.jsx
+++ src/components/home/featured/FeaturedCard.jsx
@@ -1,20 +1,30 @@
-import React from "react"
-import { featured } from "../../data/Data"
+import React, { useState } from "react";
+import { featured } from "../../data/Data";
 
 const FeaturedCard = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleIconClick = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
   return (
     <>
-      <div className='content grid5 mtop'>
-        {featured.map((items, index) => (
-          <div className='box' key={index}>
-            <img src={items.cover} alt='' />
-            <h4>{items.name}</h4>
-            <label>{items.total}</label>
+      <div className="content grid5 mtop">
+        {featured.map((item, index) => (
+          <div
+            className={`box ${activeIndex === index ? "active" : ""}`}
+            key={index}
+            onClick={() => handleIconClick(index)}
+          >
+            <img src={item.cover} alt={item.name} />
+            <h4>{item.name}</h4>
+            {activeIndex === index && <label>{item.total}</label>}
           </div>
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default FeaturedCard
+export default FeaturedCard;
